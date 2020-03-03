90a18f39882c670703a21a082ce7640d5da3c0c3
BASEL8 <basel84mn@gmail.com>
2020-03-03 14:46:25 +0100

    Updates

---
+++ precache-manifest.5dae2503f537d4aba4ad15471639d2be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96eafd5ab3682556ee07680db034e30",
+    "url": "/Wild-Nature/index.html"
+  },
+  {
+    "revision": "bc21dca1c2621ca1bf65",
+    "url": "/Wild-Nature/static/css/main.63c9bc88.chunk.css"
+  },
+  {
+    "revision": "8fabcee7e898d1af0695",
+    "url": "/Wild-Nature/static/js/2.6171f500.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/Wild-Nature/static/js/2.6171f500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc21dca1c2621ca1bf65",
+    "url": "/Wild-Nature/static/js/main.20e7691e.chunk.js"
+  },
+  {
+    "revision": "ec7dc943c38e11464dce",
+    "url": "/Wild-Nature/static/js/runtime-main.59b96285.js"
+  }
+]);
