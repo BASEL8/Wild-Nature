3e9bf615e117641eb016dddace0a52aeb36379c9
BASEL8 <basel84mn@gmail.com>
2020-03-04 10:03:37 +0100

    style fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ import { Nav, Content } from './sections'
 import styled from 'styled-components';
 const data = [
   {
-    title: 'test title',
-    subTitle: 'sub title',
-    text: 'text',
+    title: 'American fox',
+    subTitle: 'Species of america',
+    text: 'The American red fox , commonly known as the eastern American red fox, is a North American subspecies of the red fox (Vulpes vulpes). Historically, red foxes were classified as two species: Vulpes vulpes in Eurasia and Vulpes fulva in the Americas (Tesky, 1995).',
     image: 'https://images.unsplash.com/photo-1540153448870-af780343526e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
     videoLink: 'https://youtu.be/RCqQYpM1hCA'
   },
   {
-    title: 'test title',
-    subTitle: 'sub title',
-    text: 'text',
+    title: 'White-tailed deer',
+    subTitle: 'Species of america',
+    text: 'The white-tailed deer (Odocoileus virginianus), also known as the whitetail or Virginia deer, is a medium-sized deer native to North America, Central America, Ecuador',
     image: 'https://images.unsplash.com/photo-1484406566174-9da000fda645?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=935&q=80',
-    videoLink: 'https://youtu.be/RCqQYpM1hCA'
+    videoLink: 'https://www.youtube.com/watch?v=F8eCgl6JG7k'
   }
 ]
 function App() {
--- src/sections/Content/ContentData.tsx
+++ src/sections/Content/ContentData.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
+import { PlayCircle } from '@styled-icons/boxicons-regular/PlayCircle'
 
 type DataType = {
   title: string;
@@ -13,16 +14,105 @@ export const ContentData = ({ data }: { data: DataType }) => {
   console.log(data)
   return (
     <Body theme={image}>
-
+      <Top>
+        <SubTitle>
+          {subTitle}
+        </SubTitle>
+        <Title>
+          {title}
+        </Title>
+        <Text>
+          {text}
+        </Text>
+        <More>
+          <MoreVideo>
+            <PlayVideoIcon />
+            Watch Video
+          </MoreVideo>
+          <MoreInfo>
+            More Info
+          </MoreInfo>
+        </More>
+      </Top>
+      <Bottom>
+        bottom
+      </Bottom>
     </Body>
   )
 }
 const Body = styled.div`
 height:100%;
 width:100%;
-filter: brightness(0.55);
-background-image:url(${({ theme }) => theme});
+background-image: url(${({ theme }) => theme});
 background-repeat:no-repeat;
 background-position:center;
 background-size:cover;
+display:flex;
+align-items:flex-start;
+justify-content:flex-end;
+flex-direction:column;
+position:relative;
+&::after{
+  content:'';
+  position:absolute;
+  top:0;
+  left:0;
+  display:block;
+  width:100%;
+  height:100%;
+  background-color:black;
+  opacity:0.5;
+  z-index:3
+}
+`
+const Top = styled.div`
+z-index:5;
+color:white;
+padding:20px;
+text-transform: uppercase;
+width:30%;
+@media(max-width: 767px){
+    width:100%;
+  }
+`
+const Title = styled.h1`
+margin: 15px 0;
+
+`
+const SubTitle = styled.div`
+
+`
+const Text = styled.div`
+text-transform: none;
+`
+const More = styled.div`
+margin-top:15px;
+display:flex;
+align-items:center;
+justify-content:space-between;
+text-transform:none;
+font-size:12px;
+`
+const MoreVideo = styled.div`
+display:flex;
+align-items:center;
+justify-content:center;
+`
+const PlayVideoIcon = styled(PlayCircle)`
+background-color:#F39129;
+color:white;
+border-radius:50%;
+width:30px;
+padding:0;
+margin-right:20px;
+&:last-child :first-child{
+  fill:#F39129;
+};
+&:last-child :last-child{
+  width:30px;
+}
+`
+const MoreInfo = styled.div``
+const Bottom = styled.div`
+
 `
